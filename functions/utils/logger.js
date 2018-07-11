3715b8b428d58a94f133d99fb06fa0a11ccf4070
DavidWells <hello@davidwells.io>
2018-07-10 18:20:34 -0700

    add custom logger

---
+++ functions/utils/logger.js
@@ -0,0 +1,12 @@
+export default function customLogger (tokens, req, res) {
+	const log = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+
+  console.log(log)
+  return log
+}
